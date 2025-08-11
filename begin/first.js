2cb26af1452396859b5f678e9fd772c3377206a2
Hafssa-ANBR <hafssa.2017m5@gmail.com>
2025-08-11 19:29:23 +0100

    variables in js

--- begin/first.js
+++ begin/first.js
@@ -1,1 +1,6 @@
 console.log('Hello, World!')
+let itemPrice = 10.25 // var itemPrice = 10.25
+itemPrice = 11.50 // change the value of the variable
+// print a variable
+const PI = 3.14159;
+console.log(PI);
